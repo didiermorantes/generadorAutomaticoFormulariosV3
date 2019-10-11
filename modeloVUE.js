a25c4f8c2d1dacd8fde08f4f7c9dc61cfe8b68b7
didiermorantes <didiermorantes@gmail.com>
2019-10-11 15:48:02 -0500

    modelo vue actualizado

--- modeloVUE.js
+++ modeloVUE.js
@@ -1,11 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue.js!'
-    },
-    methods: {
-      reverseMessage: function () {
-        this.message = this.message.split('').reverse().join('')
-      }
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
     }
-  })
+  });
