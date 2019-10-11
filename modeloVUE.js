09cb18f210419001e9874b9bed17fd12c5500c93
didiermorantes <didiermorantes@gmail.com>
2019-10-11 16:02:21 -0500

    archivo recuperaJson.js

--- modeloVUE.js
+++ modeloVUE.js
@@ -1,3 +1,32 @@
+/*
+LA CLAVE ES OBTENER EL JSON MEDIANTE EL SCRIPT recuperaJson.js, Y LUEGO ALMACENAR EL RESULTADO EN LOCALSTORAGE
+LUEGO, modeloVUE.js , IMPORTAMOS EL OBJETO ALMACENADO EN LOCALSTORGAE Y ALMACENARLO EN EL OBJETO VUE
+
+*/
+if (typeof(Storage) !== "undefined") {
+
+	//convertimos en string en JSON para poderlo intercambiar con los servidores
+	/*
+
+	A common use of JSON is to exchange data to/from a web server.
+
+	When sending data to a web server, the data has to be a string.
+
+	Convert a JavaScript object into a string with JSON.stringify().
+
+	*/
+	var stringRecuperadoLocalStorage = localStorage.getItem('jsonAStringTransferido'); //recuperamos el json almacenado en localstorage
+
+	var stringConvertidoAObjeto=JSON.parse(stringRecuperadoLocalStorage); //Convertimos de nuevo el string en objeto javascript para ser manipulado
+	console.log("Mensaje de main.js: el string convertido a objeto recibido por localstorage es: ");
+	console.log(stringConvertidoAObjeto);
+
+
+
+} else {
+	
+  document.getElementById("demo").innerHTML = "Sorry, your browser does not support Web Storage...";
+}
 var app = new Vue({
     el: 'main',
     data: {
---
+++ recuperaJson.js
@@ -0,0 +1,45 @@
+
+var url='jsonDatos.json';
+fetch(url,{
+	headers: {
+		"Content-Type": "application/json"
+			}
+		 }
+	)
+.then(respuesta =>{
+	
+	return respuesta.json();
+})
+.then(datos=>{
+	console.log("Mensaje de myJs. Los datos obtenidos por fetch son : ");
+	console.dir( datos );
+	console.log("Mensaje de myJs. El tipo de datos del elemento recibido por fetch es : ");
+    console.log(typeof datos);
+    
+
+    var jsonRecibidoEnObjeto=datos;//guardamos en jsonRecibidoObjeto el objeto recuperado por fetch
+    
+
+
+
+
+					//OJO: la función .stringify() no funcionará de manera paralela con .parse() si stringify() no se aplica a un objeto json
+					//si  stringify() se aplica a un string, la función .parse() no devolverá un objeto
+	var objetoConvertidoaString = JSON.stringify(jsonRecibidoEnObjeto);
+
+		//mostramos el elemento recuperado y convertido en string en la seccion demo
+	
+	//debe habilitarse en el index el elemento demo para vsualizar el contendo
+	//	document.getElementById("demo").innerHTML = objetoConvertidoaString;
+
+    
+        //A workaround can be to stringify your object before storing it, and later parse it when you retrieve it:
+				  // Almacenamos en localStorage el json convertido a String
+	localStorage.setItem('jsonAStringTransferido', objetoConvertidoaString);
+
+})
+.catch( e => {
+	 
+	 console.error( 'Mensaje de myJs.js. Algo salio mal. Error en la siguiente linea: ' ) ;
+	 console.log(e);
+	 });
